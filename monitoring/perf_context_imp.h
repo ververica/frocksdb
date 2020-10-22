d6f50f33064f1d24480dfb3c586a7bd7a7dbac01
Yun Tang <myasuka@live.com>
2020-10-23 00:24:04 +0800

    [FLINK-19710] Revert implementation of PerfContext back to __thread to avoid performance regression

--- monitoring/perf_context_imp.h
+++ monitoring/perf_context_imp.h
@@ -16,7 +16,7 @@ extern PerfContext perf_context;
 extern __thread PerfContext perf_context_;
 #define perf_context (*get_perf_context())
 #else
-extern thread_local PerfContext perf_context;
+extern __thread PerfContext perf_context;
 #endif
 #endif
 
